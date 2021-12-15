4acd2d2948ea166e4d58853edd30d0bbbe13d7ec
sisirrag <mca549@rajagiri.edu>
2021-12-16 00:00:54 +0530

    database

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.io.*;
+import java.sql.*;
 
 class Main
 {
@@ -35,11 +37,26 @@ class Main
         return x;
     }
 
+    
+
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int input=sc.nextInt(); 
-        System.out.println(calc(input));    
+        String categorycode=sc.next();
+
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","system","sisir");
+        Statement st=con.createStatement();
+
+         ResultSet rs=st.executeQuery("select categorycode,value from fdb where categorycode='"+categorycode+"'");
+            while(rs.next())
+            {
+                
+                value=rs.getInt("value");
+            }
+            value=calc(value);
+            
+        st.executeUpdate("update fdb set value="+value+"where categorycode='"+categorycode+"'");
 
     }
 }
